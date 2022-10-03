675a93e23fab8f452e76ffb1c635bc1fe347b01e
raposamillar <lisa.raposamillar@gmail.com>
2022-10-03 19:57:23 -0400

    update Post model; add post routes and seeds

--- models/Post.js
+++ models/Post.js
@@ -19,13 +19,6 @@ Post.init(
       type: DataTypes.TEXT,
       allowNull: false
     },
-    post_url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isURL: true
-      }
-    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
