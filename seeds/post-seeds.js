0aa5a395974cdb9507502b730b3801d27c64d232
raposamillar <lisa.raposamillar@gmail.com>
2022-10-03 20:57:13 -0400

    update post-seeds.js with dates

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,12 +4,14 @@ const postdata = [
   {
     title: 'Why MVC is so important',
     content: 'MVC allows developers to maintain a true separation of concerns, devising their code between the Model layer for data, the View layer for design, and the Controller layer for application logic.',
-    username: 'marktwain'
+    username: 'marktwain',
+    date: 10/1/2022
   },
   {
     title: 'Authentication vs. Authorization',
     content: 'There is a difference between authentication and authorization.  Authentication means confirming your own identity, whereas authorization means being allowed access to the system.',
-    username: 'sherlockholmes'
+    username: 'sherlockholmes',
+    date: 10/2/2022
   }
 ]
 
