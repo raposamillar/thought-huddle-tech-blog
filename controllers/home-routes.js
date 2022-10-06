2d6c34afbdfff3f03f2700398142099cd61520f3
raposamillar <lisa.raposamillar@gmail.com>
2022-10-05 23:12:17 -0400

    add missing slash on line 72

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -69,7 +69,7 @@ router.get('/post/:id', (req, res) => {
     });
 });
 
-router.get('login', (req, res) => {
+router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
